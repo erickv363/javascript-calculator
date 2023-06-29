41fd58c3f400bb3a4900cb21f12e67b420b5fcc5
Erick Velasco <erickv_24@yahoo.com>
2023-06-28 20:26:56 -0400

    added array to do operations with bigger numbers

--- scripts.js
+++ scripts.js
@@ -9,14 +9,25 @@ let secondNum
 let operation
 let result
 let step = 0
+const allNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let myArray = []
 
 // 1. need an global array - []
-
+// 2. we need to keep putting numbers the global array - [1,4,6] - how? push, unshift, splice
+// 3. then we condense all the numbers in the array - 146 - how? 
+// 4. then we could save that big number into firstNum = 146 - ok
 
 function getNumber(num) {
-    // 2. we need to keep putting numbers the global array - [1,4,6] - how? 
-    // 3. then we condense all the numbers in the array - 146 - how? 
-    // 4. then we could save that big number into firstNum = 146 - ok
+    myArray.push(num)  //adding new numbers to myArray
+    let y = myArray.push(num)
+    console.log(y)
+    console.log(myArray.join(''))  //
+    const x = myArray.join('')  //join method to combine numners in from myArray into a string, then creating x varibale for it
+    firstNum = Number(x)  //number method to change x string into number, assigning firstNum to it
+    console.log(typeof x)
+    console.log(Number(firstNum))
+    console.log('firstNum', typeof firstNum)
+    
     if (step === 0) {
         firstNum = num
         step = 1
@@ -31,16 +42,13 @@ function getOperator(opps) {
     console.log(opps)
     operation = opps
     step = 2
+
+    document.getElementById("display").value = opps
 }
 
-/* function getAdd(){
-    firstNum + secondNum
-    // let result = firstNum+secondNum
-
-} */
 
 function equals() {
-    // console.log(firstNum+5)
+
     if (operation === '+') {
         result = firstNum + secondNum
     } else if (operation === '-') {
