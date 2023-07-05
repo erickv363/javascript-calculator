c848e4ab5f0f5de33e188c3024506a77dcdc785d
Erick Velasco <erickv_24@yahoo.com>
2023-07-05 18:56:36 -0400

    cleaned up some files

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-// array methods
-// myArray.pop()
-// myArray.filter()
-// myArray.map()
-// myArray.reduce()
-
 let firstNum
 let secondNum
 let operation
@@ -13,12 +7,6 @@ let firstArray = []
 let secondArray = []
 let display = document.getElementById("display")
 
-// 1. need an global array - []
-// 2. we need to keep putting numbers the global array - [1,4,6] - how? push, unshift, splice
-// 3. then we condense all the numbers in the array - 146 - how? 
-// 4. then we could save that big number into firstNum = 146 - ok
-
-// 5. finalize the calculator with > 1 digits using array push/join & Number convertor
 
 function getNumber(num) {
     if (step === 0 || step === 1) {
@@ -31,14 +19,6 @@ function getNumber(num) {
         secondNum = Number(secondArray.join(''))
         display.value = secondNum
     }
-
-    // myArray.push(num)  //adding new numbers to myArray using the push method, then creating y variable for it
-    // myArray.join('')  //join method to combine numbers in myArray into a string, then creating jointNum varibale for it 
-    // Number(myArray)
-
-    // console.log(num)
-    // display.value = num
-    // display.value = myArray
 }
 
 function getOperator(opps) {
@@ -60,7 +40,7 @@ function equals() {
         result = firstNum / secondNum
     }
 
-    display.value = result // re-asising 
+    display.value = result 
 }
 
 function getClear() {
