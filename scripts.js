d4f161d85afc9ef2c21da5e40776cad66d1ab933
Erick Velasco <erickv_24@yahoo.com>
2023-07-12 20:03:39 -0400

    created readme file

--- scripts.js
+++ scripts.js
@@ -7,49 +7,46 @@ let firstArray = []
 let secondArray = []
 let display = document.getElementById("display")
 
-
 function getNumber(num) {
-    if (step === 0 || step === 1) {
-        firstArray.push(num) // adding number to array 
-        step = 1 
-        firstNum = Number(firstArray.join(''))
-        display.value = firstNum
-    } else if (step === 2) {
-        secondArray.push(num)
-        secondNum = Number(secondArray.join(''))
-        display.value = secondNum
-    }
+  if (step === 0 || step === 1) {
+    firstArray.push(num)
+    step = 1
+    firstNum = Number(firstArray.join(""))
+    display.value = firstNum
+  } else if (step === 2) {
+    secondArray.push(num)
+    secondNum = Number(secondArray.join(""))
+    display.value = secondNum
+  }
 }
 
 function getOperator(opps) {
-    operation = opps
-    step = 2
-    display.value = opps
+  operation = opps
+  step = 2
+  display.value = opps
 }
 
-
 function equals() {
+  if (operation === "+") {
+    result = firstNum + secondNum
+  } else if (operation === "-") {
+    result = firstNum - secondNum
+  } else if (operation === "*") {
+    result = firstNum * secondNum
+  } else if (operation === "/") {
+    result = firstNum / secondNum
+  }
 
-    if (operation === '+') {
-        result = firstNum + secondNum
-    } else if (operation === '-') {
-        result = firstNum - secondNum
-    } else if (operation === '*') {
-        result = firstNum * secondNum
-    } else if (operation === '/') {
-        result = firstNum / secondNum
-    }
-
-    display.value = result 
+  display.value = result
 }
 
 function getClear() {
-    step = 0
-    firstNum = 0
-    secondNum = 0
-    result = 0
-    firstArray = []
-    secondArray = []
-    operation = null
-    display.value = result
+  step = 0
+  firstNum = 0
+  secondNum = 0
+  result = 0
+  firstArray = []
+  secondArray = []
+  operation = null
+  display.value = result
 }
